16f5c31346b0b2f52029cd953213767876f77370
me-js-bro <me.js.bro@gmail.com>
2024-06-26 18:29:13 +0600

    update: Added script to switch wallpaper engine between hyprpaper and swww. Updated scripts for switching wallpaper

---
+++ config/hypr/scripts/change_wallpaper_engine.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Wallpaper engine changing script (swww/hyprpaper)
+
+scripts="$HOME/.config/hypr/scripts"
+cache_dir="$HOME/.config/hypr/.cache"
+engine="$cache_dir/.engine"
+mkdir -p "$cache_dir"
+touch "$engine"
+
+# Function to display the prompt
+prompt() {
+    echo "swww"
+    echo "hyprpaper"
+}
+
+rofi_command="rofi -i -dmenu -config ~/.config/rofi/themes/config-wall-engine.rasi"
+
+# Main function
+main() {
+    choice=$(prompt | ${rofi_command})
+    
+    # Debugging: Output the choice to see what was selected
+    echo "Selected choice: '$choice'"
+    
+    # If nothing is selected, exit without changing anything
+    if [ -z "$choice" ]; then
+        echo "No choice selected, exiting..."
+        exit 0
+    fi
+
+    case "$choice" in
+        "swww")
+            echo "swww" > "$engine"
+            killall hyprpaper
+            ;;
+        "hyprpaper")
+            echo "hyprpaper" > "$engine"
+            killall swww-daemon
+            ;;
+        *)
+            echo "Invalid choice"
+            exit 1
+            ;;
+    esac
+
+    # Debugging: Output the content of the engine file
+    echo "Engine set to: $(cat $engine)"
+}
+
+main && "$scripts/Wallpaper.sh"
--- config/hypr/scripts/pywal.sh
+++ config/hypr/scripts/pywal.sh
@@ -1,32 +1,54 @@
 #!/bin/bash
-# Define the path to the swww cache directory
-cache_dir="$HOME/.cache/swww/"
-# Get a list of monitor outputs
-monitor_outputs=($(ls "$cache_dir"))
-# Initialize a flag to determine if the ln command was executed
-ln_success=false
-# Loop through monitor outputs
-for output in "${monitor_outputs[@]}"; do
-    # Construct the full path to the cache file
-    cache_file="$cache_dir$output"
 
-    # Check if the cache file exists for the current monitor output
-    if [ -f "$cache_file" ]; then
-        # Get the wallpaper path from the cache file
-        wallpaper_path=$(cat "$cache_file")
+# _______________________________________________________________ #
 
-        # Copy the wallpaper to the location Rofi can access
-        if ln -sf "$wallpaper_path" "$HOME/.config/hypr/.cache/current_wallpaper.png"; then
-            ln_success=true  # Set the flag to true upon successful execution
+# Define directories and files
+swww_cache_dir="$HOME/.cache/swww"
+hypr_cache_dir="$HOME/.config/hypr/.cache"
+engine_file="$hypr_cache_dir/.engine"
+current_wallpaper="$hypr_cache_dir/current_wallpaper.png"
+
+engine=$(cat "$engine_file")
+
+if [[ "$engine" = "swww" ]]; then
+    echo "$engine"
+    # Define the path to the swww cache directory
+    cache_dir="$HOME/.cache/swww/"
+    # Get a list of monitor outputs
+    monitor_outputs=($(ls "$cache_dir"))
+    # Initialize a flag to determine if the ln command was executed
+    ln_success=false
+    # Loop through monitor outputs
+    for output in "${monitor_outputs[@]}"; do
+        # Construct the full path to the cache file
+        cache_file="$cache_dir$output"
+
+        # Check if the cache file exists for the current monitor output
+        if [ -f "$cache_file" ]; then
+            # Get the wallpaper path from the cache file
+            wallpaper_path=$(cat "$cache_file")
+
+            # Copy the wallpaper to the location Rofi can access
+            if ln -sf "$wallpaper_path" "$HOME/.config/hypr/.cache/current_wallpaper.png"; then
+                ln_success=true  # Set the flag to true upon successful execution
+            fi
+
+            break  # Exit the loop after processing the first found monitor output
         fi
+    done
 
-        break  # Exit the loop after processing the first found monitor output
+    # Check the flag before executing further commands
+    if [ "$ln_success" = true ]; then
+        wal -i "$wallpaper_path"
     fi
-done
-
-# Check the flag before executing further commands
-if [ "$ln_success" = true ]; then
-    wal -i "$wallpaper_path"
+elif [[ "$engine" = "hyprpaper" ]]; then
+    echo "$engine"
+    current_wallpaper="$HOME/.config/hypr/.cache/current_wallpaper.png"
+    if [[ -f "$current_wallpaper" ]]; then
+        wal -i "$current_wallpaper"
+    else
+        echo "No $current_wallpaper found"
+    fi
 fi
 
 # Function to convert hex color code to rgba
