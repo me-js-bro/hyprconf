eb5cf262e60e3301de6b9be2d03cacf9120b585a
me-js-bro <me.js.bro@gmail.com>
2024-06-29 20:57:42 +0600

    Update: Updated startup.sh script so that it can use the Hyprpaper engine

--- config/hypr/scripts/startup.sh
+++ config/hypr/scripts/startup.sh
@@ -4,7 +4,9 @@ mode_file="$HOME/.mode"
 scripts_dir="$HOME/.config/hypr/scripts"
 wallpaper_dir="$HOME/.config/hypr/Wallpaper"
 wallpaper="$HOME/.config/hypr/.cache/current_wallpaper.png"
-ibus_path=$HOME/.cache/ibus-layout
+engine_path="$HOME/.config/hypr/.cache/.engine"
+
+engine=$(cat $engine_path)
 
 # Transition config
 FPS=60
@@ -17,9 +19,27 @@ if [ ! -f "$mode_file" ]; then
     touch "$mode_file" && echo "light" >> "$mode_file"
     "$scripts_dir/toggle_dark_light.sh"
 elif [ -f "$wallpaper" ]; then
-    swww query || swww init && swww img ${wallpaper} $SWWW_PARAMS
-    "$scripts_dir/pywal.sh"
-    "$scripts_dir/Refresh.sh"
+
+    if [[ "$engine" == "swww" ]] then
+
+        swww query || swww init && swww img ${wallpaper} $SWWW_PARAMS
+        
+    elif [[ "$engine" == "hyprpaper" ]]; then
+
+        # Ensure hyprpaper is running
+        if ! pgrep -x hyprpaper > /dev/null; then
+        hyprpaper -c ~/.config/hypr/hyprpaper.conf &
+        sleep 1  # give hyprpaper some time to start
+        fi
+
+        hyprctl hyprpaper preload "$wallpaper"
+        sleep 0.5
+        hyprctl hyprpaper wallpaper " ,$wallpaper"
+        hyprctl reload
+        
+        rm -rf ~/.cache/wal
+        
+    fi
 else
     "$scripts_dir/Wallpaper.sh"
 fi
@@ -30,3 +50,6 @@ if [[ -d "/usr/share/openbangla-keyboard" ]]; then
 fi
 
 "$scripts_dir/notification.sh" sys
+sleep 0.5
+"$scripts_dir/pywal.sh"
+"$scripts_dir/Refresh.sh"
