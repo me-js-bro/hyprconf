940cec2413c9ba69563e85e50d75833d75110339
me-js-bro <me.js.bro@gmail.com>
2024-12-26 00:12:03 +0600

    updated rofi themes. ready for a test

--- config/hypr/scripts/waybar-layout.sh
+++ config/hypr/scripts/waybar-layout.sh
@@ -11,7 +11,7 @@ waybar_style="$HOME/.config/waybar/style.css"
 script_dir="$HOME/.config/hypr/scripts"
 window_rules="$HOME/.config/hypr/configs/environment.conf"
 rofi_config="$HOME/.config/rofi/themes/rofi-waybar.rasi"
-rofi_menu="$HOME/.config/rofi/menu/style-4.rasi"
+rofi_menu="$HOME/.config/rofi/menu/menu.rasi"
 rofi_clipboard="$HOME/.config/rofi/themes/rofi-clipboard.rasi"
 environment_config="$HOME/.config/hypr/configs/environment.conf"
 
@@ -36,7 +36,7 @@ apply_config() {
     ln -sf "$layout_file" "$waybar_config"
     ln -sf "$style_file" "$waybar_style"
 
-    if [[ "$1" == "fancy-top" || "$1" == "fancy-bottom" || "$1" == "colorful-bottom" || "$1" == "colorful-top" || "$1" == "full-top-1" || "$1" == "full-bottom-1" ]]; then
+    if [[ "$1" == "fancy-top" || "$1" == "colorful-bottom" || "$1" == "full-top" ]]; then
         echo "Enabling blur in $window_rules"
         sed -i "/^#blurls = waybar$/ s/#//" "$window_rules"
         sed -i "/^#blurls = waybar$/d" "$window_rules"
@@ -54,6 +54,7 @@ apply_config() {
         sed -i "s/location:.*/location: northEast;/g" "$rofi_clipboard"
         sed -i "s/anchor:.*/anchor: northeast;/g" "$rofi_clipboard"
         sed -i "s/y-offset:.*/y-offset: 15px;/g" "$rofi_clipboard"
+        sed -i "s/x-offset:.*/x-offset: -15px;/g" "$rofi_clipboard"
     elif [[ "$1" == *"-bottom"* ]]; then
         echo "bottom"
         sed -i "s/location:.*/location: southWest;/g" "$rofi_menu"
