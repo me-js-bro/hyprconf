14b5ea473df59eaa516fde115243c7e11844cffd
me-js-bro <me.js.bro@gmail.com>
2025-01-06 22:47:16 +0600

    updated scripts, ready for a test

---
+++ config/hypr/.cache/colors/arch-colors/colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P012121b
+                 \e]P156708E
+                 \e]P2607F9F
+                 \e]P3698CAF
+                 \e]P4759DC3
+                 \e]P57BA5CD
+                 \e]P683B1DA
+                 \e]P7bcd4ea
+                 \e]P88394a3
+                 \e]P956708E
+                 \e]PA607F9F
+                 \e]PB698CAF
+                 \e]PC759DC3
+                 \e]PD7BA5CD
+                 \e]PE83B1DA
+                 \e]PFbcd4ea
+                 \ec'
--- setup.sh
+++ setup.sh
@@ -311,7 +311,6 @@ if [[ -d "$HOME/.config/hypr/Wallpaper" ]]; then
     # setting the default wallpaper
     ln -sf "$wallpaper" "$HOME/.config/hypr/.cache/current_wallpaper.png"
     echo "wallpaper = ,~/.config/hypr/.cache/current_wallpaper.png" > "$HOME/.config/hypr/hyprpaper.conf"
-    "$HOME/.config/hypr/scripts/pywal.sh" &> /dev/null
 fi
 
 # setting up the waybar
